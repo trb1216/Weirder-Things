81a8fcf65c1ab3c07d742653351c9d87ee507501
trb1216 <trb1216@gmail.com>
2021-07-29 01:46:32 -0500

    New front page, new logo,  new register and login components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import logo from "./components/weirder-things.png";
+import "./App.css";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src={logo} className="App-image" alt="weider-things-logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" /> */}
+        <p>{/* Edit <code>src/App.js</code> and save to reload. */}</p>
+        {/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and jS and jsx and othersss
-        </a>
+          /Learn React
+        </a>*/}
+        <Register />
+        <Login />
       </header>
     </div>
   );
---
+++ src/components/Register.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <button>register</button>;
+};
+
+export default Register;
