a0bfb28993e00c9a7810e0bffcc54ad08110f834
malgorze <gosia.popowska@gmail.com>
2021-07-31 10:03:36 -0500

    BrowserRouter udpate

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+    <button>Home</button>
+    <button> Profile</button>
+    <button>Post
+  </button>)
+};
+
+export default Register;
