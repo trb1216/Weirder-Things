79bd373e189265b45dcebd2b776082e7a377a659
malgorze <gosia.popowska@gmail.com>
2021-07-31 11:40:58 -0500

    navbar routing testing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 // import logo from './logo.svg';
-import logo from "./components/weirder-things.png";
 import "./App.css";
+import { Switch, Route } from "react-router-dom"
+import logo from "./components/weirder-things.png";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import {fetchAllPosts} from "./api/api.js";
-import Navbar from "./components/Navbar.js/index.js"
+import {Navbar, Home, Profile, Posts} from "./components/Navbar.js/index.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-image" alt="weider-things-logo" />
-        <Navbar />
+      </header>
+      <Navbar />
+        <Switch>
+          <Route path="/home" component ={Home} />
+          <Route path="/profile" component ={Profile} />
+          <Route path="/posts" component ={Posts} />
+        </Switch>
         <Register />
         <Login />
         <fetchAllPosts />
-      </header>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-    <button>Home</button>
-    <button> Profile</button>
-    <button>Post
-  </button>)
-};
+    return (<div>
+        <Link to="/home">
+        <button>Home</button>
+        </Link>
+        <Link to="/posts">
+        <button>Posts</button>
+        </Link>
+        <Link to="/profile">
+        <button>Profile</button>
+        </Link>
+        <Link to="/">
+        <button>Log Out</button>
+        </Link>
+        </div>);
+}
 
-export default Register;
+export default Navbar;
