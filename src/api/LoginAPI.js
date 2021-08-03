c2b061273e626b3505f34b862ab472d4be01036b
Tim <trb1216@gmail.com>
2021-08-03 15:23:44 -0500

    Add files via upload

---
+++ src/api/LoginAPI.js
@@ -0,0 +1,22 @@
+const LOGIN_URL =
+    "https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-PT/users/login";
+
+  const userLogin = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user: {
+        username: "test",
+        password: "test",
+      },
+    }),
+  };
+
+  fetch(LOGIN_URL, userLogin)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+    return <div><h1>Home Content</h1></div>
+};
+
+export default Home;
