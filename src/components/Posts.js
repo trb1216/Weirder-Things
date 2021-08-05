7d1112b2bc23eed614e8f8bf5be748a2d6c8c12b
trb1216 <trb1216@gmail.com>
2021-08-05 02:14:25 -0500

    Fixed and cleaned up API's. They are now in working order. Deleted LoginAPI and PostsAPI as they areno longer needed. stopped the 10,000 f*@$ng messages coming through the console that was crashing the browser.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ReactDOM } from "react";
+const POSTS_URL =
+  "https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-PT/posts";
 
 const Posts = () => {
-    return <div><h1>Posts</h1></div>
+  const [body, setBody] = useState([]);
+  const userPost = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer TOKEN_STRING_HERE",
+    },
+    body: JSON.stringify({
+      /* whatever things you need to send to the API */
+    }),
+  };
+  useEffect(() => {
+    fetch(POSTS_URL, userPost)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setBody(json);
+      });
+  }, []);
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setBody(body);
+        }}
+      >
+        <input
+          type="text"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></input>
+      </form>
+    </div>
+  );
 };
 
 export default Posts;
