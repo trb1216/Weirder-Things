a9bda59073b68cadac77d0abc272f0ace311e866
malgorze <gosia.popowska@gmail.com>
2021-08-07 10:37:53 -0500

    one more

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 // import logo from './logo.svg';
 import "./App.css";
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route } from "react-router-dom";
 import logo from "./components/weirder-things.png";
-import { LoginPage, Navbar, Home, Profile, Posts, Register, Login } from "./components";
+import {
+  LoginPage,
+  Navbar,
+  Home,
+  Profile,
+  Posts,
+  Register,
+  Login,
+} from "./components";
 
 function App() {
-  // if loggedOUt then to Login
-  // if (userIsLoggedIn === false)
-  //   return (
-  //     <div className="App">
-  //       <Switch>
-  //         <Route path="/register">
-  //           <Register></Register>
-  //         </Route>
-  //         <Route default>
-  //           <Login></Login>
-  //         </Route>
-  //       </Switch>
-  //     </div>
-  //   );
+  if (localStorage.getItem("user")) {
+    return (
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/posts" component={Posts} />
+        </Switch>
+      </div>
+    );
+  }
 
   return (
     //login intro//
@@ -31,24 +37,14 @@ function App() {
           alt="weider-things-logo"
         />
       </header>
-      <main>
-        <div>
-          <LoginPage />
-          <Switch>
-            <Route path="/register" component={Register} />
-            <Route path="/" component={Login} />
-          </Switch>
-        </div>
-        {/* navigation, small logo, page style */}
-        <div>
-          <Navbar />
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/posts" component={Posts} />
-          </Switch>
-        </div>
-      </main>
+      <div>
+        <LoginPage />
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/" component={Login} />
+        </Switch>
+      </div>
+      {/* navigation, small logo, page style */}
     </div>
   );
 }
