45c63563eba663db5a6314a32023edbd18d836be
malgorze <gosia.popowska@gmail.com>
2021-08-07 09:40:27 -0500

    small fix

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,11 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< Updated upstream
-import Register from "./Register";
-=======
-import Login from "./Login.jsx";
-import Register from "./Register.jsx";
->>>>>>> Stashed changes
 
 const LoginPage = () => {
   return (
