9f4f7612eb44d11b61b96dd57a5e27feb0609e81
trb1216 <trb1216@gmail.com>
2021-08-10 15:35:49 -0500

    Home fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,53 +1,56 @@
-import React from "react";
-import { userInfo } from "../api/index";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { userMessages } from "../api/index";
 
 const userToken = localStorage.getItem("userToken");
 
-const Home = ({ username, posts, messages, _id }) => {
+const Home = ({ username, posts, _id }) => {
+  const [messages, setMessages] = useState();
   useEffect(() => {
-    userInfo(userToken);
+    userMessages(userToken).then(setMessages);
   }, []);
 
   return (
     <div>
       <h1>Welcome Home {username}!</h1>
       <h2>Posts</h2>
-
-      {posts.map((post) => {
-        return (
-          <div className="Postings" key={post._id}>
-            <h4>{post.title}</h4>
-            <p>
-              Who: <b>{post.author}</b>
-            </p>
-            <p>
-              When: <b>{post.createdAt}</b>
-            </p>
-            <p>
-              What: <b>{post.description}</b>
-            </p>
-            <p>
-              Price: <b>{post.price}</b>
-            </p>
-          </div>
-        );
-      })}
+      {posts && posts.length > 0
+        ? posts.map((post) => {
+          return (
+            <div className="Postings" key={post._id}>
+              <h4>{post.title}</h4>
+              <p>
+                Who: <b>{post.author}</b>
+              </p>
+              <p>
+                When: <b>{post.createdAt}</b>
+              </p>
+              <p>
+                What: <b>{post.description}</b>
+              </p>
+              <p>
+                Price: <b>{post.price}</b>
+              </p>
+            </div>
+          );
+        })
+        : "Loading..."}
 
       <h2>Messages</h2>
-      {messages.map((message) => {
-        return (
-          <div className="Postings" key={message._id}>
-            <h4>{message.post.title}</h4>
-            <p>
-              From: <b>{message.fromUser.username}</b>
-            </p>
-            <p>
-              Message: <b>{message.content}</b>
-            </p>
-          </div>
-        );
-      })}
+      {messages && messages.length > 0
+        ? messages.map((message) => {
+          return (
+            <div className="Postings" key={message._id}>
+              <h4>{message.post.title}</h4>
+              <p>
+                From: <b>{message.fromUser.username}</b>
+              </p>
+              <p>
+                Message: <b>{message.content}</b>
+              </p>
+            </div>
+          );
+        })
+        : "Loading..."}
     </div>
   );
 };
